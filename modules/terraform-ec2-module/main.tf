b96285ad62757594ba652b5918a998a512b43def
grsuresh4u <86867233+grsuresh4u@users.noreply.github.com>
2026-05-22 01:19:53 +0530

    feat(terraform-ec2-module): add main.tf

---
+++ modules/terraform-ec2-module/main.tf
@@ -0,0 +1,85 @@
+terraform {
+  required_version = ">= 1.5"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0"
+    }
+  }
+}
+
+resource "aws_iam_role" "this" {
+  name = var.iam_role_name
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal = { Service = "ec2.amazonaws.com" }
+        Action    = "sts:AssumeRole"
+      }
+    ]
+  })
+
+  tags = var.tags
+}
+
+resource "aws_iam_role_policy_attachment" "ecr_readonly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.this.name
+}
+
+resource "aws_iam_role_policy_attachment" "ssm" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+  role       = aws_iam_role.this.name
+}
+
+resource "aws_iam_role_policy_attachment" "cloudwatch" {
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchLogsFullAccess"
+  role       = aws_iam_role.this.name
+}
+
+resource "aws_iam_role_policy_attachment" "extra" {
+  count      = length(var.extra_policy_arns)
+  policy_arn = var.extra_policy_arns[count.index]
+  role       = aws_iam_role.this.name
+}
+
+resource "aws_iam_instance_profile" "this" {
+  name = var.iam_role_name
+  role = aws_iam_role.this.name
+}
+
+resource "aws_instance" "this" {
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  subnet_id              = var.subnet_id
+  vpc_security_group_ids = var.security_group_ids
+  iam_instance_profile   = aws_iam_instance_profile.this.name
+  user_data              = var.user_data != "" ? var.user_data : null
+  key_name               = var.key_name != "" ? var.key_name : null
+
+  root_block_device {
+    volume_size           = var.root_volume_size
+    volume_type           = "gp3"
+    encrypted             = true
+    delete_on_termination = true
+  }
+
+  tags = var.tags
+}
+
+resource "aws_ebs_volume" "data" {
+  availability_zone = var.availability_zone
+  size              = var.data_volume_size
+  type              = "gp3"
+  encrypted         = true
+  tags              = merge(var.tags, { Name = "${lookup(var.tags, "Name", "instance")}-data" })
+}
+
+resource "aws_volume_attachment" "data" {
+  device_name = "/dev/sdf"
+  volume_id   = aws_ebs_volume.data.id
+  instance_id = aws_instance.this.id
+}
